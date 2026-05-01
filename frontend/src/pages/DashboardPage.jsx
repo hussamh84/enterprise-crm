c5416c97c473c06892822b383811d47efb68ef88
Hussam <hhammad14@gmail.com>
2026-05-01 19:14:40 +0400

    debug real dashboard component

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,20 @@
 import DashboardV2 from "./DashboardV2";
 
 export default function DashboardPage() {
-  return <DashboardV2 />;
+  return (
+    <div>
+      <div
+        id="1w40si"
+        style={{
+          background: "red",
+          color: "white",
+          padding: "20px",
+          fontSize: "40px",
+        }}
+      >
+        REAL DASHBOARD FILE
+      </div>
+      <DashboardV2 />
+    </div>
+  );
 }
